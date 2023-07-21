58a033371c91baf8beefde5ab0ff8238ab97a82c
dariodev18 <dariodev18@users.noreply.github.com>
2023-07-21 19:15:39 -0300

    archivo con error en texto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'nombre-del-proyecto';
+  mensaje = 'hola Argentina Programa 2023';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Comp1Component } from './comp1/comp1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Comp1Component
   ],
   imports: [
     BrowserModule
